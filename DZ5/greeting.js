07d58de49d6850a1b4e6b512f7f354f6e335227e
a2804 <kalysbekovadilet4@gmail.com>
2022-07-21 16:57:46 +0600

    DZ5

---
+++ DZ5/greeting.js
@@ -0,0 +1,66 @@
+const list =   [
+
+]
+
+const addButton=document.getElementById("button");
+const input = document.getElementById('input');
+
+function change(id) {
+    const item = list.findIndex(t => t.id === id)
+    const text = prompt("текст для изменения")
+    list[item].text = text;
+    render()
+}
+
+function deleteBut(id) {
+    const icon = list.findIndex(k => k.id === id)
+    list[icon].text = ""
+    render()
+}
+
+function render() {
+    const mainDiv = document.createElement('div');
+    mainDiv.setAttribute('class', 'list');
+
+    for(let i = 0; i < list.length; i++){
+        const div = document.createElement('div');
+        div.setAttribute('class', 'todoBlock');
+        const p = document.createElement('p');
+        p.innerText = list[i].text
+        div.append(p)
+
+        const buttons = document.createElement('div')
+        buttons.setAttribute("class",'actions')
+        const changeButton = document.createElement("button");
+        changeButton.setAttribute('class', 'change');
+        changeButton.onclick = ()=>{
+            change(list[i].id)
+        }
+        changeButton.innerText = 'change'
+        const deleteButton = document.createElement("button");
+        deleteButton.setAttribute("class", 'delete');
+        deleteButton.innerText = 'delete'
+        deleteButton.onclick = () => {
+            deleteBut(list[i].id)
+        }
+
+        buttons.append(changeButton, deleteButton);
+        div.append(buttons);
+        mainDiv.append(div)
+    }
+    console.log(mainDiv)
+
+    const form=document.querySelector('.form')
+    document.querySelector('.list').remove()
+    form.append(mainDiv)
+}
+
+addButton.onclick = () => {
+    const obj = {
+        id: list.length+1,
+        text: input.value
+    }
+    list.push(obj);
+    console.log(list)
+    render()
+}
